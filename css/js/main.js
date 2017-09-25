42d301c263f0c69476e902122ddbcff534d9b5a6
Godson <godsonobielum@yahoo.com>
2017-09-25 13:05:03 +0100

    Main repo now

---
+++ css/js/main.js
@@ -0,0 +1,13 @@
+$(".touch-slider").owlCarousel({
+        navigation: false,
+        pagination: true,
+        slideSpeed: 1000,
+        stopOnHover: true,
+        autoPlay: true,
+        items: 1,
+        itemsDesktopSmall: [1024, 1],
+        itemsTablet: [600, 1],
+        itemsMobile: [479, 1]
+    });
+    $('.touch-slider').find('.owl-prev').html('<i class="fa fa-chevron-left"></i>');
+    $('.touch-slider').find('.owl-next').html('<i class="fa fa-chevron-right"></i>');
